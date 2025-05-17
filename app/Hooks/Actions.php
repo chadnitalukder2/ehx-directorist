8df0c59b343b1ce726018f0f5ef8aa12e4655db2
chadnitalukder2 <chadnitalukder2@gmail.com>
2025-05-17 18:15:16 +0600

    Integrate map

--- app/Hooks/Actions.php
+++ app/Hooks/Actions.php
@@ -29,24 +29,15 @@ class Actions
             return 'Id is required';
         }
         $listingData = ListingResource::getPost($id);
-
-        wp_enqueue_script(
-            'google-maps',
-            'https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY',
-            array(),
-            null,
-            true
-        );
-     
         wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css');
         wp_enqueue_style('ehxd_listing_details', EHX_DIRECTORIST_PLUGIN_URL . 'assets/css/listing-details.css', [], EHX_DIRECTORIST_VERSION);
         wp_enqueue_script('ehxd_listing_details_js', EHX_DIRECTORIST_PLUGIN_URL . 'assets/js/listing-details.js', array('jquery'), EHX_DIRECTORIST_VERSION, false);
-        wp_localize_script('ehxd_listing_details_js', 'ehxdMapData', [
+        wp_localize_script('ehxd_listing_details_js', 'ehxdMapData', array(
             'latitude'  => $listingData['latitude'],
             'longitude' => $listingData['longitude'],
-            'address' => $listingData['address'],
+            'address'   => $listingData['address'],
             'title'     => $listingData['name'],
-        ]);
+        ));
         ob_start();
         View::render('ListingDetails/ListingDetails', [
             'data' => $listingData,
